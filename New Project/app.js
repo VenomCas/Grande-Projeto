b0b4c94ee124b46dcf465f1ee06f5a44ca727398
Venâncio Araújo <132025797+VenomCas@users.noreply.github.com>
2023-11-18 11:28:26 -0400

    Add files via upload

---
+++ New Project/app.js
@@ -0,0 +1,35 @@
+
+//    let random = Math.random();
+//if (random < 0.5){
+//    console.log("Seu numero menor que 0.5")
+//    console.log (random);
+//}
+//if (random >= 0.5){
+// console.log("Seu numero maior que 0.5")
+//   console.log (random);
+// }
+
+//const dayOfWeek = 'Sabado';
+//if (dayOfWeek === 'Monday') {
+//    console.log ("Eu odeio segunda")
+//} else if (dayOfWeek === 'Sabado'){
+//    console.log("Amo sabado!")
+//}
+
+//0-5 - FREE 
+//5 - 10 - Child R$10
+//10 - 65 - ADULT R$20
+//65+ IDOSO R$10
+
+//const age = 45;
+//
+//if (age < 5) {
+//    console.log("Vc é bebe e come de graça")
+//} else if (age < 10) {
+//    console.log("Se vc é crinaça, paga 10 conto")
+//} else if (age < 65) {
+//    console.log("você é idoso, paga 10 conto")
+//} else if (age < 50) {
+//    console.log("você é adulto, paga 10 conto")
+//}
+
